db79bb49bd39611f0892cf54a1bdd85aa2caa737
aryan singh <aryan@iitgn.ac.in>
2016-10-02 03:04:19 +0530

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+// import axios from 'axios';
+// const API_KEY = '0db05679a7c6cd52194bd072ef3877b5';
+// const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+//
+// export const FETCH_WEATHER = 'FETCH_WEATHER';
+//
+// export function fetchWeather(){
+//   const url = `${ROOT_URL}&q=${city},`;
+//   const request = axios.get(url);
+//   return {
+//     type: FETCH_WEATHER,
+//     payload: request
+//   };
+// }
