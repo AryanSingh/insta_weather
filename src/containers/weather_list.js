e91c62d332e16c1f23982685fb45a7b4c3775bab
aryan singh <aryan@iitgn.ac.in>
2016-10-04 08:44:31 +0530

    sparkline chard added and code refactored

---
+++ src/containers/weather_list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart'; 
+
+
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td>
+					<Chart data={temps} color="orange" />	
+				</td>
+			</tr>
+			);
+	}
+
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Presure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
+			);
+	}
+}
+
+function mapStateToProps({ weather }) {
+	return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
+
