8b6b9e7eaba2eb50c52bc1b78cd618dff8e3b313
aryan singh <aryan@iitgn.ac.in>
2016-10-04 06:19:18 +0530

    fetch_weather wired to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,16 @@
-// import axios from 'axios';
-// const API_KEY = '0db05679a7c6cd52194bd072ef3877b5';
-// const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-//
-// export const FETCH_WEATHER = 'FETCH_WEATHER';
-//
-// export function fetchWeather(){
-//   const url = `${ROOT_URL}&q=${city},`;
-//   const request = axios.get(url);
-//   return {
-//     type: FETCH_WEATHER,
-//     payload: request
-//   };
-// }
+import axios from 'axios';
+
+const API_KEY = '0db05679a7c6cd52194bd072ef3877b5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},in`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators} from 'redux';
-// import { fetchWeather } from '../actions/index';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
-  constructor(props){
+class SearchBar extends Component {
+  constructor(props) {
     super(props);
-    this.state = { term: ''};
+    this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
-  onInputChange(event){
-    this.setState({ term: event.target.value});
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
   }
 
-  onFormSubmit(event){
+  onFormSubmit(event) {
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
-  render(){
+  render() {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five day forecast in your favorite cities"
           value={this.state.term}
           onChange={this.onInputChange}
-          className="form-control"/>
+          className="form-control" />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
@@ -33,3 +37,9 @@ export default class SearchBar extends Component{
     );
   }
 }
+
+function mapDispatachToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatachToProps)(SearchBar);
