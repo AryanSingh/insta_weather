b77239b73ae7ff3d13b88efbc86d9937ec3b1e23
aryan singh <aryan@iitgn.ac.in>
2016-10-10 06:39:35 +0530

    express added and bundle.js removed from gitignore and webpack -p removed from packae.json

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '0db05679a7c6cd52194bd072ef3877b5';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
